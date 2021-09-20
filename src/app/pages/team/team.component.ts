208723a6357cebab1872d42cc04664ce25309dfb
alexc777 <osmancruz187@gmail.com>
2021-09-19 18:38:25 -0600

    array de proyectos para landing page y listado de proyectos, array de categorias y re nombremiento de imagenes

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss']
 })
-export class TeamComponent implements OnInit {
+export class TeamComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
