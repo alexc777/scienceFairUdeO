0ed846a7fe62fd2119b27394b4bee819a1543eef
alexc777 <osmancruz187@gmail.com>
2021-09-23 12:53:51 -0600

    Lista de proyectos
    se actualizaron lo proyectos y modal para login

--- src/app/pages/list-projects/list-projects.component.ts
+++ src/app/pages/list-projects/list-projects.component.ts
@@ -34,11 +34,11 @@ export class ListProjectsComponent implements OnInit {
         description: 'Sistema desarrollado con tecnologías web y .Net que permite la exposición de proyectos y asignar una calificación'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/covid-temperatura/covid-temp.png',
         category: 'elec_progra',
         name: 'COVID Monitoreo de Temperatura',
         slug: 'covid-monitoreo-temperatura',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Es un medidor de temperatura corporal, el cual tiene como función el brindar una ayuda para llevar un monitoreo controlado y eficaz debido a la situación actual del COVID.'
       },
       {
         imagen: 'assets/img/veh-detector/veh-detector.jpg',
@@ -69,11 +69,11 @@ export class ListProjectsComponent implements OnInit {
         description: 'Eliminación de bacterias de agua contaminada utilizando luces led ultravioleta'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/solar-geogebra-b/solar-b.png',
         category: 'naturales',
         name: 'Sistema planetaria GeoGebra Grupo B',
         slug: 'sistema-planetario-grupo-b',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Animación de sistema solar con imágenes de los planetas y modelo 3D el cual fue creado por medio de GeoGebra software de matemáticas dinámicas libre para todas las áreas de las matemáticas'
       },
       {
         imagen: 'assets/img/goldberg-b/golfberg.png',
@@ -97,7 +97,7 @@ export class ListProjectsComponent implements OnInit {
         description: 'El proyecto a realizar será un videojuego, que se desarrollará en PHP7, Canvas y MySQL, el cual contará con un personaje principal, que será un doctor que se encontrará luchando contra el virus para poder salvarse y lograr la meta final, que consistirá en atravesar los 3 niveles que estarán formando este videojuego'
       },
       {
-        imagen: 'assets/img/casa-domotica/casa-domótica.jpg',
+        imagen: 'assets/img/casa-domotica/casa-domotica.jpg',
         category: 'electronica',
         name: 'Casa domótica',
         slug: 'casa-domotica',
@@ -125,25 +125,25 @@ export class ListProjectsComponent implements OnInit {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/goldberg-c/golberg-c.jpg',
         category: 'naturales',
         name: 'Máquina de Goldberg 40 pasos Grupo C',
         slug: 'maquina-de-bolberg-grupo-c',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Una máquina de Rube Goldberg es cualquier aparato muy complejo que realiza una tarea muy simple de una manera muy indirecta.'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/robot3-1/robot3-1.jpg',
         category: 'electronica',
         name: 'Robot 3 en 1 con Arduino, vehiculo autónomo digital',
         slug: 'robot-3-en-1-con-arduino',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'El siguiente proyecto consiste en la programación y elaboración de un robot móvil autónomo que estará siendo realizado con una tarjeta basada en microcontrolador de la familia Arduino. Su finalidad es que tenga movimiento en su trayectoria marcada, nos basaremos en un trabajo eficiente de evasión de obstáculos. Para el mismo se realiza un análisis de los requisitos tanto a nivel hardware como software que deberá contener el robot. Adicionalmente contará con un circuito de estacionamiento para el recorrido del mismo.'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/brazo/brazo.png',
         category: 'electronica',
         name: 'Brazo robótico',
         slug: 'brazo-robotico',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Es un brazo robótico capaz de poder moverse por sí solo, ya sea por medio de pulsadores, potenciómetros o consigo mismo poder ser controlado por algún contro'
       },
       {
         imagen: 'assets/img/llenado-pinturas/proyecto-pintura.jpeg',
@@ -153,11 +153,11 @@ export class ListProjectsComponent implements OnInit {
         description: 'Es un prototipo para un sistema autónomo que se basa en el llenado de envases de pinturas el cual es monitoreado contantemente por medio de sensores digitales el cual es procesado y enviado vía Radio Frecuencias a un panel central el cual es gestionado y enviado auna BD(Base De Datos) y así poder monitorear contantemente los productos terminados, dependiendo del código de barras detectado así será su color dispensado, cabe resaltar que es un circuito con detección de errores lo que quiere decir que si hay algún problema en el sistema el dispositivo nos lo indicara atreves de sus distintas señalizaciones.'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/ally-cane/ally-cane.png',
         category: 'elec_progra',
         name: 'Ally Cane/Baston Aliado',
         slug: 'baston-aliado',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Somos un grupo de estudiante universitarios y a grandes rasgos buscamos hacer más fácil la adaptación de movilizarse de una persona que haya perdido la vista ya sea por accidente o enfermedad y también dar un adicional de comodidad a las personas que nacieron sin la vista. Queremos brindarles la mayor seguridad a la hora de movilizarse por la ciudad, debido a que la infraestructura no se encuentra adaptado a un 100% para este tipo de capacidades, dado a que el bastón cuenta con sensores ultrasónicos que le permite aproximar y alertar en el momento de topar con algún objeto o persona. Brindar apoyo con la ubicación satelital (GPS) debido que, como cualquier  otra persona, necesita cubrir sus necesidades personales lo cual lo implica salir y en algunas situaciones implica ir a un lugar que no conoce, el GPS lo guiara con mayor facilidad para encontrar su destino, dándole así seguridad al movilizarse. Ayudar grandemente con la contaminación ambiental, debido a que cuenta con paneles solares que cargaran la batería sin necesidad de  conectarla a una corriente eléctrica, para así disminuir el impacto de  consumo energético y el gasto de baterías desechables.'
       },
       {
         imagen: 'assets/img/banner-projects.jpg',
@@ -167,11 +167,11 @@ export class ListProjectsComponent implements OnInit {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/propeller/propeller.jpg',
         category: 'electronica',
         name: 'Propeller',
         slug: 'propeller',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Nuestro proyecto consiste en un motor que hace girar una base, la cuál, consta de un Arduino, un módulo Bluetooth y una matriz LED. El objetivo de nuestro proyecto es desplegar texto y diseños de figuras, aplicando conocimientos de electrónica y programación ya que, al mismo tiempo, quisimos desarrollarlo de una manera interactiva. Como todos sabemos los cables están quedando obsoletos, por lo tanto, hemos hecho uso de un modulo para controlarlo a través de una aplicación y así cargar la información a nuestra matriz LED'
       },
       {
         imagen: 'assets/img/banner-projects.jpg',
@@ -188,11 +188,11 @@ export class ListProjectsComponent implements OnInit {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/solar-geogebra-c/solar-c.png',
         category: 'naturales',
         name: 'Sistema planetario GeoGebra Grupo C',
         slug: 'sistema-planetario-grupo-c',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'El proyecto consiste en una animación del sistema solar por medio del programa Geogebra que es unsoftware que nos permite realizar procesos geométricos, algebraicos y cálculos y visualizar graficas quenos permite ver desde otro punto de vista las aplicaciones que nos brinda la geometría para poder realizar mencionado proyecto.'
       },
       {
         imagen: 'assets/img/banner-projects.jpg',
@@ -202,11 +202,11 @@ export class ListProjectsComponent implements OnInit {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/pdf/pdf-scap.png',
         category: 'naturales',
         name: 'PDF SCRAPING',
         slug: 'pdf-scraping',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'El web Scraping es una herramienta bastante útil utilizada para extraer información de distintas páginas web desde el código fuente. Una vez extraída la información, se almacena en la base de datos del programa donde es procesada y compilada en una tabla, la cual se exporta como distintos archivos, en este caso, será exportado en PDF para poder abrir el documento y ver la recolección del Scraping de cualquier tipo de información. En el proyecto recopilaremos la información de distintas páginas web para hacer una o varias tablas con distintos temas como: Futbol, PIB, Índice de población en países, etc.'
       },
       {
         imagen: 'assets/img/sis-comandas/siscom.png',
