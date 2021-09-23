34c53e53f78d7665e0810235c7b8bf00a70d7d87
alexc777 <osmancruz187@gmail.com>
2021-09-22 20:00:19 -0600

    actualización lista de proyectos

--- src/app/pages/list-projects/list-projects.component.ts
+++ src/app/pages/list-projects/list-projects.component.ts
@@ -41,11 +41,11 @@ export class ListProjectsComponent implements OnInit {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/veh-detector/veh-detector.jpg',
         category: 'electronica',
         name: 'Vehículo detector de obstáculos',
         slug: 'vehiculo-detector-de-obstaculos',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Robot controlado por módulo bluetooth programado por microcontrolador Arduino UNO para seguir líneas de color negro dentro de una pista, esquivar obstáculos que existan frente al robot mientras realiza su recorrido y detectar humo proveniente de gases colocados alrededor de la pista.'
       },
       {
         imagen: 'assets/img/banner-projects.jpg',
@@ -55,18 +55,18 @@ export class ListProjectsComponent implements OnInit {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/solar-geogebra-a/sistema-solar-a.png',
         category: 'naturales',
         name: 'Sistema planetario GeoGebra Grupo A',
         slug: 'sistema-planetario-grupo-a',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Es plano o diagrama realizado por medio de Geogebra basado en el sistema solar, el proceso del mismo nos conlleva a usar coordenadas y análisis matemático, como lógico.'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/elimina-bacterias/elimina-bac.PNG',
         category: 'naturales',
         name: 'Eliminacion de Bacterias en agua para consumo humano',
         slug: 'eliminacion-de-backterias-en-agua',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Eliminación de bacterias de agua contaminada utilizando luces led ultravioleta'
       },
       {
         imagen: 'assets/img/banner-projects.jpg',
@@ -76,46 +76,46 @@ export class ListProjectsComponent implements OnInit {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/goldberg-b/golfberg.png',
         category: 'naturales',
         name: 'Máquina de Goldberg 40 pasos Grupo B',
         slug: 'maquina-de-goldberg-grupo-b',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'La máquina Consiste en Realizar una Tarea cotidiana de una forma diferente y complicada, con una serie de obstáculos y niveles.'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/centros-vacunacion/c-vacunacion.png',
         category: 'elec_progra',
         name: 'Centros de Vacunación',
         slug: 'centros-de-vacunacion',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Aplicación web en la cual un usuario obtenga la ubicación de su centro de vacunación más cercano y que cumpla con sus preferencias de vacunación luego de pasar por un filtro en el cual indique su grupo social de acuerdo a las opciones de registro de vacunación proporcionadas por el Gobierno de Guatemala.'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/videoj-covid/covid-19.jpeg',
         category: 'elec_progra',
         name: 'Video Juego - Salvémonos del COVID-19',
         slug: 'video-juego-covid',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'El proyecto a realizar será un videojuego, que se desarrollará en PHP7, Canvas y MySQL, el cual contará con un personaje principal, que será un doctor que se encontrará luchando contra el virus para poder salvarse y lograr la meta final, que consistirá en atravesar los 3 niveles que estarán formando este videojuego'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/casa-domotica/casa-domótica.jpg',
         category: 'electronica',
         name: 'Casa domótica',
         slug: 'casa-domotica',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Casa domótica controlada por aplicación en teléfono inteligente y con enlace tipo BLUETOOTH. Consiste en el conjunto de técnicas orientadas a la automatización en una vivienda'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/botonetas/botonetas.png',
         category: 'naturales',
         name: 'Sistema automático para separar botonetas por color',
         slug: 'sistema-botonetas',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Sistema automatizado con Arduino Uno para detectar el color de objetos y colocarlos en recipientes separados'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/euler/euler.png',
         category: 'naturales',
         name: 'Resolución del Método Euler',
         slug: 'resolucion-euler',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'El proyecto consiste en a realización de un solucionador automatizado que calculara y graficar el método de Euler mediante algoritmos de Analítica Avanzada de la Ciencia de Datos.'
       },
       {
         imagen: 'assets/img/banner-projects.jpg',
@@ -146,11 +146,11 @@ export class ListProjectsComponent implements OnInit {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/llenado-pinturas/proyecto-pintura.jpeg',
         category: 'elec_progra',
         name: 'Llenado en la selección automática de envases en Fábrica de Pinturas',
         slug: 'llenado-envases-pintura',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
+        description: 'Es un prototipo para un sistema autónomo que se basa en el llenado de envases de pinturas el cual es monitoreado contantemente por medio de sensores digitales el cual es procesado y enviado vía Radio Frecuencias a un panel central el cual es gestionado y enviado auna BD(Base De Datos) y así poder monitorear contantemente los productos terminados, dependiendo del código de barras detectado así será su color dispensado, cabe resaltar que es un circuito con detección de errores lo que quiere decir que si hay algún problema en el sistema el dispositivo nos lo indicara atreves de sus distintas señalizaciones.'
       },
       {
         imagen: 'assets/img/banner-projects.jpg',
@@ -209,11 +209,11 @@ export class ListProjectsComponent implements OnInit {
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent volutpat, tortor eget accumsan pulvinar'
       },
       {
-        imagen: 'assets/img/banner-projects.jpg',
+        imagen: 'assets/img/sis-comandas/siscom.png',
         category: 'elec_progra',
         name: 'SISCOM (Sistema de Comandas)',
         slug: 'siscom',
-        description: 'Eco Restaurante San Luis, ubicado en Aldea El Hato, Antigua Guatemala ofrece un concepto al aire libre, rodeado de naturaleza.El proyecto SISCOM surge en el momento que los meseros toman la orden a los comensales, anotando en una libreta la orden.En ocasiones, las ordenes tienden a ser confundidas al momento de servirlas debido a que no se da el seguimiento adecuado entre orden y número de mesa durante los tiempos de alta demanda de servicio.'
+        description: 'Eco Restaurante San Luis, ubicado en Aldea El Hato, Antigua Guatemala ofrece un concepto al aire libre, rodeado de naturaleza. El proyecto SISCOM surge en el momento que los meseros toman la orden a los comensales, anotando en una libreta la orden. En ocasiones, las ordenes tienden a ser confundidas al momento de servirlas debido a que no se da el seguimiento adecuado entre orden y número de mesa durante los tiempos de alta demanda de servicio. Por lo cual, el proyecto consiste en realizar la toma de ordenes por medio de un dispositivo móvil Android.'
       }
     ];
 
